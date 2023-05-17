434eb8dc34b2dcaa1265121ce722d823c6902dd6
Echa <theresiajuli2018@gmail.com>
2023-05-17 08:33:19 +0700

    tugas

--- Character.java
+++ Character.java
@@ -2,4 +2,8 @@ public class Character {
     public void move(){
         System.out.println("Character Move!!");
     }
+
+    public void ultimateMove(){
+        
+    }
 }
--- Fighter.java
+++ Fighter.java
@@ -1,3 +1,7 @@
 public class Fighter extends Hero{
     public void move(){}
+
+    public void ultimateMove(){
+        //implementasi bebas   
+    }
 }
